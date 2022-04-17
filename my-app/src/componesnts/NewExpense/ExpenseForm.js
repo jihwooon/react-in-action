45d2e7801ca917028a23177bea6efb6b7741c4b6
jihwooon <jihwooon@gmail.com>
2022-04-17 23:45:40 +0900

    feat : State 대신 사용하기(그리고 더 나은 방법)

--- my-app/src/componesnts/NewExpense/ExpenseForm.js
+++ my-app/src/componesnts/NewExpense/ExpenseForm.js
@@ -3,20 +3,38 @@ import React, {useState} from "react";
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState('');
-  const [enteredAmount, setEnteredAmount] = useState('');
-  const [enterDate, setEnteredDate] = useState('');
+  // const [enteredTitle, setEnteredTitle] = useState('');
+  // const [enteredAmount, setEnteredAmount] = useState('');
+  // const [enterDate, setEnteredDate] = useState('');
+
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enterDate: ''
+  });
 
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    })
   };
 
   const amountChangeHandler = event => {
-    setEnteredAmount(event.target.value);
+    // setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    })
   };
 
   const dateChangeHandler = event => {
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enterDate: event.target.value,
+    })
   };
 
   return (
