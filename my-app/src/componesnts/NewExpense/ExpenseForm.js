fd919b81ee4fbdb8ff8e795319319a87207db07a
jihwooon <jihwooon@gmail.com>
2022-04-18 00:30:52 +0900

    feat : 양식 제출 처리

--- my-app/src/componesnts/NewExpense/ExpenseForm.js
+++ my-app/src/componesnts/NewExpense/ExpenseForm.js
@@ -3,61 +3,57 @@ import React, {useState} from "react";
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState('');
-  // const [enteredAmount, setEnteredAmount] = useState('');
-  // const [enterDate, setEnteredDate] = useState('');
-
-  const [userInput, setUserInput] = useState({
-    enteredTitle: '',
-    enteredAmount: '',
-    enterDate: ''
-  });
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enterDate, setEnteredDate] = useState('');
 
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
-    })
-  };
+    setEnteredTitle(event.target.value);
 
-  const amountChangeHandler = event => {
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
-    })
-  };
+    const amountChangeHandler = (event) => {
+      setEnteredAmount(event.target.value);
 
-  const dateChangeHandler = event => {
-    // setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      enterDate: event.target.value,
-    })
-  };
+    };
 
-  return (
-      <form>
-        <div className="new-expense__controls">
-          <div className="new-expense__control">
-            <label>Title</label>
-            <input type='text' onChange={titleChangeHandler}/>
+    const dateChangeHandler = (event) => {
+      setEnteredDate(event.target.value);
+
+    };
+
+    const submitHandler = (event) => {
+      event.preventDefault();
+
+      const expenseDate = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enterDate)
+      };
+
+      console.log(expenseDate);
+    };
+
+    return (
+        <form onSubmit={submitHandler}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input type='text' onChange={titleChangeHandler}/>
+            </div>
           </div>
-        </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler}/>
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input type='date' min="2019-01-01" step="=2022-12-31" onChange={dateChangeHandler}/>
-        </div>
-        <div className='new-expense__actions'>
-          <button type='submit'>Add Expense</button>
-        </div>
-      </form>
-  );
+          <div className="new-expense__control">
+            <label>Amount</label>
+            <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler}/>
+          </div>
+          <div className="new-expense__control">
+            <label>Date</label>
+            <input type='date' min="2019-01-01" step="=2022-12-31" onChange={dateChangeHandler}/>
+          </div>
+          <div className='new-expense__actions'>
+            <button type='submit'>Add Expense</button>
+          </div>
+        </form>
+    );
+  };
 };
 
 export default ExpenseForm;
