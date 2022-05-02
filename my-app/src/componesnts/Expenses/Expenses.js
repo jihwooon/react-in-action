b5a5e5ae3ef176143053d363baab1509db93af10
jihwooon <jihwooon@gmail.com>
2022-05-02 23:08:33 +0900

    feat : 연습하기 List 다루기

--- my-app/src/componesnts/Expenses/Expenses.js
+++ my-app/src/componesnts/Expenses/Expenses.js
@@ -11,6 +11,11 @@ const Expenses = (props) => {
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   }
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
       <div>
         <Card className="expenses">
@@ -18,8 +23,9 @@ const Expenses = (props) => {
               selected={filteredYear}
               onChangeFilter={filterChangeHandler}
           />
-          {props.items.map((expense) => (
+          {filteredExpenses.map((expense) => (
               <ExpenseItem
+                  key={expense.id}
                   title={expense.title}
                   amount={expense.amount}
                   date={expense.date}
