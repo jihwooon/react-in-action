27b35fa06051fa34f7f01a7dbccbcc521405ba64
jihwooon <jihwooon@gmail.com>
2022-07-23 00:55:18 +0900

    State 저장 목록 사용

--- 3-react-rendering-conditional/src/App.js
+++ 3-react-rendering-conditional/src/App.js
@@ -1,23 +1,30 @@
+import React, {useState} from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./NewExpense/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {id: 'e1', title: 'Total Paper', amount: 94.12, date: new Date(2021, 2, 28)},
+  {id: 'e2', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 3, 27)},
+  {id: 'e3', title: 'Car Existence', amount: 256.12, date: new Date(2021, 4, 15)},
+  {id: 'e4', title: 'Paper Company', amount: 100.12, date: new Date(2022, 3, 20)}
+];
+
+
 const App = () => {
-  const expenses = [
-    {id: 'e1', title: 'Total Paper', amount: 94.12, date: new Date(2021, 2, 28)},
-    {id: 'e2', title: 'Car Insurance', amount: 294.67, date: new Date(2021, 3, 27)},
-    {id: 'e3', title: 'Car Existence', amount: 256.12, date: new Date(2021, 4, 15)},
-    {id: 'e4', title: 'Paper Company', amount: 100.12, date: new Date(2022, 3, 20)}
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
 
   const addExpenseHandler = expenses => {
-    console.log('In App.js')
-    console.log(expenses)
-  }
+    setExpenses(prevExpenses => {
+      return [expenses, ...prevExpenses]
+    });
+  };
 
   return (
+
       <div>
-        <NewExpense onAddExpense = {addExpenseHandler}/>
-        <Expenses items={expenses}/>
+        <NewExpense onAddExpense={addExpenseHandler}/>
+        <Expenses items={DUMMY_EXPENSES}/>
       </div>
   );
 }
