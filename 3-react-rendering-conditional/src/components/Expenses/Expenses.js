15daa9f8e2278589863fff9c73807b61f07b9115
jihwooon <jihwooon@gmail.com>
2022-07-23 01:24:50 +0900

    조건부 내용 출력하기

--- 3-react-rendering-conditional/src/components/Expenses/Expenses.js
+++ 3-react-rendering-conditional/src/components/Expenses/Expenses.js
@@ -1,19 +1,41 @@
+import React, {useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses Fou</p>
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+        <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+        />
+    ));
+  }
+
   return (
       <div>
         <Card className='expenses'>
-          {props.items.map((expense) =>
-              <ExpenseItem
-                  key={expense.id}
-                  title={expense.title}
-                  amount={expense.amount}
-                  date={expense.date}
-              />
-          )}
+          <ExpensesFilter
+              selected={filteredYear}
+              onChangeFilter={filterChangeHandler}
+          />
+          {expensesContent}
         </Card>
       </div>
   );
